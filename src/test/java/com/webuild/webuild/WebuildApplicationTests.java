dfa295c4a7550f90ca0ea127018e1580c97c44ca
blueilla <jasenszky.ilona@gmail.com>
2018-10-16 10:57:28 +0200

    init (webuild webapp)create the project repository and springboot application

---
+++ src/test/java/com/webuild/webuild/WebuildApplicationTests.java
@@ -0,0 +1,16 @@
+package com.webuild.webuild;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebuildApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
